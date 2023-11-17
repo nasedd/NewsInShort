4f744f76e2150be5a798baff3a4fc37cacb7b9cc
Nadir Seddaoui <seddaoui.nadir@gmail.com>
2023-11-17 17:59:11 +0100

    Central Dependencies Management in new central gradle

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,24 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appcompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx }" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}" }
+    val composeUi by lazy { "androidx.compose.ui:ui:" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics:" }
+    val composeUiToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val composeMaterial3 by lazy { "androidx.compose.material3:material3" }
+    val navigationCompose by lazy { "androidx.navigation:navigation-compose:${Versions.navigationCompose}" }
+    val composeTestJUnit4 by lazy { "androidx.compose.ui:ui-test-junit4:${Versions.compose}" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling:${Versions.compose}" }
+    val composeUiTestManifest by lazy { "androidx.compose.ui:ui-test-manifest:${Versions.compose}" }
+    val lifecycleViewModelKtx by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleViewModelKtx }" }
+
+
+
+
+
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,11 @@
+object Versions {
+
+    const val coreKtx = "1.9.0"
+    const val appcompat = "1.9.0"
+    const val material = "1.9.0"
+    const val lifecycleRuntimeKtx = "1.9.0"
+    const val activityCompose = "1.9.0"
+    const val compose = "1.9.0"
+    const val navigationCompose = "1.9.0"
+    const val lifecycleViewModelKtx = "1.9.0"
+}
