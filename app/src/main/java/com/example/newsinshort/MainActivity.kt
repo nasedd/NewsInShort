b73e8b8a5c3cb54ab665b2d475ac3eb1b2576ce9
Nadir Seddaoui <seddaoui.nadir@gmail.com>
2023-11-19 01:35:40 +0100

    correction version bug

--- app/src/main/java/com/example/newsinshort/MainActivity.kt
+++ app/src/main/java/com/example/newsinshort/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.newsinshort
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsinshort.ui.theme.NewsInShortTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,3 +17,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun NewsInShortEntryPoint() {
+
+}
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
 
     const val coreKtx = "1.9.0"
-    const val appcompat = "1.9.0"
+    const val appcompat = "1.6.1"
     const val material = "1.9.0"
-    const val lifecycleRuntimeKtx = "1.9.0"
-    const val activityCompose = "1.9.0"
-    const val compose = "1.9.0"
-    const val navigationCompose = "1.9.0"
-    const val lifecycleViewModelKtx = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.6.1"
+    const val activityCompose = "1.7.2"
+    const val compose = "2023.03.00"
+    const val navigationCompose = "2.7.0"
+    const val lifecycleViewModelKtx = "2.6.1"
 }
